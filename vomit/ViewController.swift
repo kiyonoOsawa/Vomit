06d55aa87330783b6883f711311845723933b2d5
KiyonoOsawa <kiyo.sntr.x6@icloud.com>
2020-01-05 00:06:37 +0900

    Initial Commit

---
+++ vomit/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  vomit
+//
+//  Created by 大澤清乃 on 2020/01/05.
+//  Copyright © 2020 大澤清乃. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
